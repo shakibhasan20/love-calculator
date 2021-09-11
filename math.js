dbebe18730198350d61f277359d463ada81f1855
shakibhasan20 <90474906+shakibhasan20@users.noreply.github.com>
2021-09-11 22:39:26 +0600

    Add files via upload

---
+++ math.js
@@ -0,0 +1,14 @@
+let firstName =prompt(" Enter the Boy's name ");
+let SecondName =prompt(" Enter the Girl's name ");
+// value = Math.round(2.5);
+// value = Math.ceil(2.9);
+// value = Math.floor(2.9);
+// value = Math.sqrt(81);
+// value = Math.pow(8,2); 
+// value = Math.abs(-2); 
+// value = Math.max (-2,3,435,5,6,356,6,6); 
+// value = Math.min (-2,3,435,5,6,356,6,6); 
+let value = Math.random() * 100; 
+value = Math.floor(value) +1;
+
+alert ( firstName + " and " + SecondName + "'s love percentage is : "  + value +  "%");
